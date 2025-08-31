ae68cb41cc981d38ccc64541baafb33b0678d919
Amodhyh <35001977+amodhyh@users.noreply.github.com>
2025-09-01 02:33:52 +0530

    replaced the DTO files

---
+++ lib/src/main/java/com/sazzler/ecommerce/order_service/DTO/OrderDTO.java
@@ -0,0 +1,4 @@
+package com.sazzler.ecommerce.order_service.DTO;
+
+public record OrderDTO(String name,int id) {
+}
--- lib/src/main/java/com/sazzler/ecommerce/product_service/DTO/ProductResponse.java
+++ lib/src/main/java/com/sazzler/ecommerce/product_service/DTO/ProductResponse.java
@@ -1,12 +1,7 @@
 package com.sazzler.ecommerce.product_service.DTO;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@AllArgsConstructor
-@Data
-public class ProductResponse {
-    private Long id;
-    private String description;
-    private Double price;
+public record ProductResponse (
+     int id,
+     String description,
+     Double price) {
 }
