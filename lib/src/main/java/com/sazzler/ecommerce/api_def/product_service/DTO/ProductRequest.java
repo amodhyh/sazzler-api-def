be0d82c732967725978ebcd5c1a065d8a9df9e35
Amodhyh <35001977+amodhyh@users.noreply.github.com>
2025-09-01 16:52:39 +0530

    integrated the service registry,api gateway and the product service

--- lib/src/main/java/com/sazzler/ecommerce/api_def/product_service/DTO/ProductRequest.java
+++ lib/src/main/java/com/sazzler/ecommerce/api_def/product_service/DTO/ProductRequest.java
@@ -1,4 +1,4 @@
-package com.sazzler.ecommerce.product_service.DTO;
+package com.sazzler.ecommerce.api_def.product_service.DTO;
 
 
 public record ProductRequest(String name,int id,double price,int quantity) {
